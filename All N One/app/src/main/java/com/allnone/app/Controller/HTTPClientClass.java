5fcd13ecba931adda89b44727032d70113d0f39a
Adam McGivern <adammcgivern@outlook.com>
2015-08-06 15:09:28 +0100

    Commit 3: HTTP
    Added HTTP class with post and get methods.

---
+++ All N One/app/src/main/java/com/allnone/app/Controller/HTTPClientClass.java
@@ -0,0 +1,21 @@
+package com.allnone.app.Controller;
+import java.net.HttpURLConnection;
+import java.net.URL;
+/**
+ * Created by testing on 06/08/15.
+ */
+public class HTTPClientClass {
+
+    public HttpURLConnection myHttpclient;
+    private boolean boolCheck=false;
+
+    public boolean BXP_Mobile_Post(string function, String params){
+
+        return boolCheck;
+    }
+
+    public boolean BXP_Mobile_Get(string function){
+
+        return boolCheck;
+    }
+}
--- All N One/app/src/main/java/com/allnone/app/Models/Login.java
+++ All N One/app/src/main/java/com/allnone/app/Models/Login.java
@@ -39,5 +39,5 @@ public class Login {
         instance.strPassword = password;
 
     }
-    
+
 }
